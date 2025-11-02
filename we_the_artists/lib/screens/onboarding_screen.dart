4c0d5e97125671e28e4bc96e1c0294a0782a8968
Wakhi-Ken <w.dlamini@gmail.com>
2025-11-02 13:26:00 +0200

    homescreen

--- we_the_artists/lib/screens/onboarding_screen.dart
+++ we_the_artists/lib/screens/onboarding_screen.dart
@@ -1,15 +1,47 @@
 import 'package:flutter/material.dart';
 
-class OnboardingScreen extends StatefulWidget {
+class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
 
   @override
-  State<OnboardingScreen> createState() => _OnboardingScreenState();
-}
-
-class _OnboardingScreenState extends State<OnboardingScreen> {
-  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome to We The Artists!',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Discover amazing artworks and connect with artists around the world.',
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacementNamed(
+                  context,
+                  '/home',
+                ); // Navigate to Home
+              },
+              child: const Text('Get Started'),
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 50,
+                  vertical: 15,
+                ),
+                backgroundColor: Colors.blue,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
